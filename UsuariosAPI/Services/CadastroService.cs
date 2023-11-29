6845f2de73c91372d037ce018f7e9b9db78e3b6b
Mauro <maurojr_@outlook.com>
2023-11-28 23:33:42 -0300

    Criando serviço para cadastrar usuário

---
+++ UsuariosAPI/Services/CadastroService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using UsuariosAPI.Data.Dtos;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Services
+{
+    public class CadastroService
+    {
+        private IMapper _mapper;
+        private UserManager<Usuario> _userManager;
+
+        public CadastroService(UserManager<Usuario> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        public async Task CadastraAsync(CreateUsuarioDto dto)
+        {
+            Usuario usuario = _mapper.Map<Usuario>
+               (dto);
+
+            IdentityResult resultado = await _userManager.CreateAsync(usuario, dto.Password);
+
+            if (resultado.Succeeded)
+            {
+                throw new ApplicationException("Falha ao cadastrar usuário!");
+            }
+            
+        }
+    }
+}
